ae91928768e1d9286778ccc78c5ea262f7393b80
acarioni <alessandro.carioni@lightstreamer.com>
2021-09-14 11:48:01 +0200

    delete objective-c files

--- StockList Demo for macOS-Prefix.swift
+++
@@ -1,7 +0,0 @@
-//  Converted to Swift 5.4 by Swiftify v5.4.25812 - https://swiftify.com/
-//
-// Prefix header for all source files of the 'StockList Demo for OS X' target in the 'StockList Demo for OS X' project
-//
-
-import Cocoa
-import Lightstreamer_macOS_Client
